9fc112679019cc714c75a34ed6af8089474f2aaa
owTnitraM <martin.n2bt@gmail.com>
2026-01-13 10:21:20 -0300

    ficha pg1

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-const viewportWidth = window.innerWidth;
-const viewportHeight = window.innerHeight;
-
-// Function to calculate the Greatest Common Divisor (GCD)
-function gcd(a, b) {
-    return b === 0 ? a : gcd(b, a % b);
+let c = {
+    name: 'Carlos Matthew da Silva',
+    max_life: 56,
+    current_life: 56,
+    class: 'Mago',
+    lvl: 6,
+    proficiency: 3,
+    race: 'Human',
+    movement: '9m/6q',
+    alignment: "Chaotic Neutral",
+    player: "2nitraM",
+    att: {
+        for: 9,
+        des: 14,
+        con: 18,
+        int: 20,
+        sab: 14,
+        car: 11
+    },
+    saves: {
+        for: false,
+        des: false,
+        con: false,
+        int: true,
+        sab: true,
+        car: false
+    },
+    skills: {
+        acrobacia: false,
+        arcanismo: true,
+        atletismo: false,
+        atuacao: false,
+        blefar: false,
+        furtividade: false,
+        historia: true,
+        intimidacao: false,
+        intuicao: true,
+        investigacao: false,
+        lidar_com_animais: false,
+        medicina: true,
+        natureza: false,
+        percepcao: true,
+        persuasao: false,
+        prestidigitacao: false,
+        religiao: false,
+        sobrevivencia: false
+    }
 }
-
-function getViewportAspectRatio() {
-    const commonDivisor = gcd(viewportWidth, viewportHeight);
-    const aspectRatioWidth = viewportWidth / commonDivisor;
-    const aspectRatioHeight = viewportHeight / commonDivisor;
-    const aspectRatio = `${aspectRatioWidth}:${aspectRatioHeight}`;
-
-    console.log(`Viewport Aspect Ratio: ${aspectRatio}`);
-    return aspectRatio;
-}
-
-function minhaFuncao() {
-    document.getElementById('a').innerText = getViewportAspectRatio();
-}
-
-// const meuTimer = setInterval(minhaFuncao, 100);
-
-
-
-
